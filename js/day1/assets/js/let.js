7bfa34fb71776f7103f4b1c1b4eced394465dc15
kishore <kishoreece2004@gmail.com>
2026-06-08 22:27:42 +0530

    updated all

---
+++ js/day1/assets/js/let.js
@@ -0,0 +1,60 @@
+let downfall
+console.log(downfall)
+let photoshoot
+console.log(photoshoot)
+let bikeride
+console.log(bikeride)
+let backlog
+console.log(backlog)
+let totallymesmerized
+console.log(totallymesmerized)
+let volleyball
+console.log(volleyball)
+let favouriteplaylist
+console.log(favouriteplaylist)
+let internship
+console.log(internship)
+let breakingbad
+console.log(breakingbad)
+let mothersday
+console.log(mothersday)
+let orangepink
+console.log(orangepink)
+let chennaisuperking
+console.log(chennaisuperking)
+let linkedinprofile
+console.log(linkedinprofile)
+let heartattack
+console.log(heartattack) 
+let demonslayer
+console.log(demonslayer)
+let egoisticparent
+console.log(egoisticparent)
+let tastysambar
+console.log(tastysambar)
+let chennaicity
+console.log(chennaicity)
+let goodnight
+console.log(goodnight)
+let oppositeparty
+console.log(oppositeparty)
+let spritualmode
+console.log(spritualmode)
+let waffleshop
+console.log(waffleshop)
+let googlemap
+console.log(googlemap)
+let nostalogicmoment
+console.log(nostalogicmoment)
+let moneymatter
+console.log(moneymatter)
+let differenthobby
+console.log(differenthobby)
+let babysitter
+console.log(babysitter)
+let banjeejump
+console.log(banjeejump)
+let semesterexamshedule
+console.log(semesterexamshedule)
+let fullstackdevolper
+console.log(fullstackdevolper)
---
+++ js/day1/assets/js/variable.js
@@ -0,0 +1,40 @@
+var freefire;
+console.log(freefire)
+var goodday
+console.log(goodday)
+var angrybird
+console.log(angrybird)
+var summervacation
+console.log(summervacation)
+var chickenshawarma
+console.log(chickenshawarma)
+var olivegreen
+console.log(olivegreen)
+var salemanager
+console.log(salemanager)
+var followrequest
+console.log(followrequest)
+var longdistance
+console.log(longdistance)
+var bucketlist
+console.log(bucketlist)
+var collegebus
+console.log(collegebus)
+var sisterinlaw
+console.log(sisterinlaw)
+var recordwork
+console.log(recordwork)
+var birthdaywish
+console.log(brithdaywish)
+var jobreferral
+console.log(jobreferral)
+var weddingplan
+console.log(weddingplan)
+var boundedscript
+console.log(boundedscript)
+var mosquitonet
+console.log(mosquitonet)
+var mentalhealth
+console.log(mentalhealth)
+var mobiledisplay
+console.log(mobiledisplay)
