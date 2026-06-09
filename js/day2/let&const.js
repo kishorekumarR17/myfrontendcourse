cc505f46aa2d0681dcf3d5222a1552066a27328d
kishore <kishoreece2004@gmail.com>
2026-06-09 10:27:49 +0530

    updated all

---
+++ js/day2/let&const.js
@@ -0,0 +1,218 @@
+//let string
+let background ="red";
+console.log(typeof  background,"background",  background);
+let dresssize ="xl"
+console.log(typeof dresssize,"dresssize", dresssize);
+let moviename ="lik"
+console.log(typeof moviename,"moviename",moviename);
+let hairtype ="curly"
+console.log(typeof hairtype,"hairtype", hairtype)
+let gamename ="ludoking"
+console.log(typeof gamename,"gamename",gamename)
+let foodtype = "southindian"
+console.log(typeof foodtype,"foodtype",foodtype)
+let vitaminname ="vitamin D"
+console.log(typeof vitaminname,"vitaminname",vitaminname)
+let mooncolour ="sunset orange"
+console.log(typeof mooncolour,"mooncolour",mooncolour)
+let brithday ="06oct"
+console.log(typeof brithday,"birthday",brithday)
+let bikenumber ="tn25D0610"
+console.log(typeof bikenumber,"bikenumber",bikenumber)
+
+
+//let number
+
+let numberofjoker=1
+console.log(typeof numberofjoker,"numberofjoker",numberofjoker);
+let totalcgpa=6.97
+console.log(typeof totalcgpa,"totalcgpa",totalcgpa)
+let yearofexpirence=15
+console.log(typeof yearofexpirence,"yearexpirence",yearofexpirence);
+let totalBooks = 120;
+console.log(typeof totalBooks, "totalBooks", totalBooks);
+let numberOfCars = 45;
+console.log(typeof numberOfCars, "numberOfCars", numberOfCars);
+let totalEmployees = 250;
+console.log(typeof totalEmployees, "totalEmployees", totalEmployees);
+let classroomStrength = 60;
+console.log(typeof classroomStrength, "classroomStrength", classroomStrength);
+let totalTickets = 500;
+console.log(typeof totalTickets, "totalTickets", totalTickets);
+let numberOfFloors = 12;
+console.log(typeof numberOfFloors, "numberOfFloors", numberOfFloors);
+let totalQuestions = 100;
+console.log(typeof totalQuestions, "totalQuestions", totalQuestions);
+let completedTasks = 35;
+console.log(typeof completedTasks, "completedTasks", completedTasks);
+let totalOrders = 780;
+console.log(typeof totalOrders, "totalOrders", totalOrders);
+let availableRooms = 28;
+console.log(typeof availableRooms, "availableRooms", availableRooms);
+let totalPlayers = 22;
+console.log(typeof totalPlayers, "totalPlayers", totalPlayers);
+let numberOfComputers = 90;
+console.log(typeof numberOfComputers, "numberOfComputers", numberOfComputers);
+let totalChapters = 18;
+console.log(typeof totalChapters, "totalChapters", totalChapters);
+let numberOfBranches = 7;
+console.log(typeof numberOfBranches, "numberOfBranches", numberOfBranches);
+let productCount = 300;
+console.log(typeof productCount, "productCount", productCount);
+let availableSeats = 75;
+console.log(typeof availableSeats, "availableSeats", availableSeats);
+let totalMarks = 450;
+console.log(typeof totalMarks, "totalMarks", totalMarks);
+
+
+//let boolean
+
+let moonisvisibleinevening = true
+console.log(typeof moonisvisibleinevening,"moonvisibleinevening",moonisvisibleinevening)
+let ispetrolprizerasied = true
+console.log(typeof ispetrolprizerasied,"ispetrolprizerasied",ispetrolprizerasied)
+let willitraintoday = false
+console.log(willitraintoday,"willitraintoday",willitraintoday)
+let doesthesalarycredited =false
+console.log(doesthesalarycredited,"doesthesalarycredited",doesthesalarycredited)
+let junkfoodareunhealthy =false
+console.log(typeof junkfoodareunhealthy,"junkfoodareunhealthy",junkfoodareunhealthy)
+
+
+
+
+//const number
+const totalproduct= 300;
+console.log(typeof totalproduct,"totalproduct",totalproduct)
+
+const totalStudents = 550;
+console.log(typeof totalStudents, "totalStudents", totalStudents);
+
+const monthlySalary = 45000;
+console.log(typeof monthlySalary, "monthlySalary", monthlySalary);
+
+const totalCountries = 195;
+console.log(typeof totalCountries, "totalCountries", totalCountries);
+
+const cricketScore = 287;
+console.log(typeof cricketScore, "cricketScore", cricketScore);
+
+const laptopPrice = 65000;
+console.log(typeof laptopPrice, "laptopPrice", laptopPrice);
+
+const busCapacity = 52;
+console.log(typeof busCapacity, "busCapacity", busCapacity);
+
+const movieDuration = 180;
+console.log(typeof movieDuration, "movieDuration", movieDuration);
+
+const mobileStorage = 256;
+console.log(typeof mobileStorage, "mobileStorage", mobileStorage);
+
+const totalVisitors = 1200;
+console.log(typeof totalVisitors, "totalVisitors", totalVisitors);
+
+const examQuestions = 75;
+console.log(typeof examQuestions, "examQuestions", examQuestions);
+
+const trainCoaches = 24;
+console.log(typeof trainCoaches, "trainCoaches", trainCoaches);
+
+const hospitalBeds = 350;
+console.log(typeof hospitalBeds, "hospitalBeds", hospitalBeds);
+
+const websiteUsers = 5000;
+console.log(typeof websiteUsers, "websiteUsers", websiteUsers);
+
+const apartmentFloors = 15;
+console.log(typeof apartmentFloors, "apartmentFloors", apartmentFloors);
+
+const footballPlayers = 11;
+console.log(typeof footballPlayers, "footballPlayers", footballPlayers);
+
+const libraryBooks = 25000;
+console.log(typeof libraryBooks, "libraryBooks", libraryBooks);
+
+const parkingSlots = 180;
+console.log(typeof parkingSlots, "parkingSlots", parkingSlots);
+
+const factoryWorkers = 420;
+console.log(typeof factoryWorkers, "factoryWorkers", factoryWorkers);
+
+const deliveryOrders = 950;
+console.log(typeof deliveryOrders, "deliveryOrders", deliveryOrders);
+
+const youtubeSubscribers = 100000;
+console.log(typeof youtubeSubscribers, "youtubeSubscribers", youtubeSubscribers);
+
+//const string
+const studentName = "naveenkumar";
+console.log(typeof studentName, "studentName", studentName);
+
+const collegeName = "saveetha Engineering College";
+console.log(typeof collegeName, "collegeName", collegeName);
+
+const cityName = "Chennai";
+console.log(typeof cityName, "cityName", cityName);
+
+const countryName = "India";
+console.log(typeof countryName, "countryName", countryName);
+
+const favoriteFood = "Biryani";
+console.log(typeof favoriteFood, "favoriteFood", favoriteFood);
+
+const favoriteColor = "Blue";
+console.log(typeof favoriteColor, "favoriteColor", favoriteColor);
+
+const courseName = "JavaScript";
+console.log(typeof courseName, "courseName", courseName);
+
+const companyName = "Tech Solutions";
+console.log(typeof companyName, "companyName", companyName);
+
+const emailId = "nvk123@gmail.com";
+console.log(typeof emailId, "emailId", emailId);
+
+const phoneBrand = "Samsung";
+console.log(typeof phoneBrand, "phoneBrand", phoneBrand);
+
+const movieTitle = "Leo";
+console.log(typeof movieTitle, "movieTitle", movieTitle);
+
+const bookTitle = "Atomic Habits";
+console.log(typeof bookTitle, "bookTitle", bookTitle);
+
+const programmingLanguage = "Python";
+console.log(typeof programmingLanguage, "programmingLanguage", programmingLanguage);
+
+const browserName = "Chrome";
+console.log(typeof browserName, "browserName", browserName);
+
+const operatingSystem = "Windows";
+console.log(typeof operatingSystem, "operatingSystem", operatingSystem);
+
+const trainerName = "Karthik";
+console.log(typeof trainerName, "trainerName", trainerName);
+
+const departmentName = "Computer Science";
+console.log(typeof departmentName, "departmentName", departmentName);
+
+const projectName = "Student Portal";
+console.log(typeof projectName, "projectName", projectName);
+
+const stateName = "Tamil Nadu";
+console.log(typeof stateName, "stateName", stateName);
+
+const websiteName = "HealthyBites";
+console.log(typeof websiteName, "websiteName", websiteName);
+
+//const boolean
+
+const doesSlahascommunicationclass = true
+console.log(typeof doesSlahascommunicationclass, " doesSlahascommunicationclass",doesSlahascommunicationclass)
+const tajmahallocatedinagra = true
+console.log(typeof tajmahallocatedinagra,"tajamahallocatedinagra", tajmahallocatedinagra)
+const stalinwontheelection =false
+console.log(typeof stalinwontheelection,"stalinwontheelection",stalinwontheelection)
+const pythonusedinfrontend =false
+console.log(typeof pythonusedinfrontend,"pythonusedinfrontend",pythonusedinfrontend)
