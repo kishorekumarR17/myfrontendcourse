2f93c575b54d2bcf63b5e2a3954e7016e038c3a7
kishore <kishoreece2004@gmail.com>
2026-06-10 00:36:38 +0530

    updated all

---
+++ js/day3/operator.js
@@ -0,0 +1,161 @@
+//Task1 student mark calutation
+
+let English = 98
+let chemistry = 100
+let computerscience = 100
+Totalmarks = English+chemistry+computerscience
+console.log("Totalmarks",Totalmarks)
+Averagemarks = (English+chemistry+computerscience)/3
+console.log("Averagemarks",Averagemarks);
+
+
+
+
+// task2 employee salary
+
+let employeeBasicsalary = 50000
+let bonus = 5000
+let grossSalary = employeeBasicsalary+bonus
+console.log("grossSalary",grossSalary)
+
+// task3 shoping bill
+let productprize = 999
+let quantity = 4
+let totalamount = productprize *  quantity
+console.log("totalamount",totalamount)
+
+//task4 Restaurant Bill Split
+
+let totalbill = 2500 
+let friendscount = 7
+let spiltperPerson = totalbill / friendscount
+console.log("spiltperson",spiltperPerson)
+
+
+
+//task5  Even/Odd Checker Logic
+let num = 25
+let remainder = num%5
+console.log("remainder",remainder);
+
+
+
+// task6 area square 
+let side =5
+let result = side*7
+console.log("result",result)
+
+
+//task7 bank balance update
+
+let balance = 10000
+let deposit =2000
+balance += deposit
+console.log("balance",balance)
+
+//task8 ATM withdarwal
+let Currentbalance = 5000
+let withdarwal = 1000
+Currentbalance -= withdarwal
+console.log("Currentbalance",Currentbalance)
+
+//TASK9 PRODUCT STOCK UPDATE 
+
+let Totalproduct = 10
+let stockincresed = 5
+Totalproduct *= stockincresed
+console.log("totalproduct",Totalproduct);
+
+ //TASK10 DISCOUNT CALCULATION
+
+ let prizeofproduct = 2000
+ let Discount = 2
+ prizeofproduct /= Discount
+ console.log("prizeofproduct",prizeofproduct);
+
+ //Task11 login validation result 
+
+
+ let username1 = "admin"
+ let username2 = "admin"
+console.log("firstlogic",username1 == username2);
+console.log("secondlogic",username1 === username2);
+
+
+//Task12  Age verification
+
+let age = 18
+console.log(age>10);
+console.log(age<10);
+console.log(age>=10);
+console.log(age<=10);
+
+
+//Task13 Product comparison
+
+
+let price1 = 1000
+let price2 =1500
+console.log("not equal",price1!=price2);
+
+
+//Task14 College Admission Eligibility
+
+
+let mark = 85
+let hasCertificate = true
+console.log("and",mark&&hasCertificate);
+
+
+//Task15   Company Access System
+
+let isadmin = false
+let isHr = true
+let isBlocked = false 
+console.log("or",isadmin||isHr||isBlocked);
+
+console.log("not",!isadmin)
+console.log("not",!isHr)
+console.log("not",!isBlocked)
+
+// final mini project task
+
+let studentName = "naveen"
+let Computerscience =34
+let Chemistry = 70
+let Maths = 100
+let NUMBEROFSUBJECT=3
+TOTALMARKS = Computerscience+Chemistry+Maths
+AVERAGE = (Computerscience+Chemistry+Maths)/NUMBEROFSUBJECT
+console.log("studentname",studentName);
+console.log("Computerscience",Computerscience)
+console.log("Chemistry",Chemistry)
+console.log("Maths",Maths);
+
+
+
+console.log("Totalmark",TOTALMARKS);
+console.log("Average",AVERAGE);
+console.log("mark different between computerscience and Maths",Computerscience-Maths)
+console.log("percentage",TOTALMARKS/300 *100)
+console.log("+=",Computerscience+=Maths)
+console.log("-=",Computerscience-=Maths)
+console.log("*=",Computerscience*=Maths)
+console.log("/=",Computerscience/=Maths)
+console.log("==",Computerscience==Maths)
+console.log("===",Computerscience===Maths)
+console.log("!=",Computerscience!=Maths)
+console.log(">",Computerscience>Maths)
+console.log("<",Computerscience<Maths)
+console.log(">=",Computerscience>=Maths)
+console.log("<=",Computerscience<=Maths)
+
+
+
+
+
+
+
+
+
+
